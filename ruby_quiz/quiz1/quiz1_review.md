9c0ccff36c2704b4d27ca43fc32876c7a6cd6ade
LittleNoah <ljw57011001@sina.com>
2016-03-05 22:43:36 +0800

    sleep today

--- ruby_quiz/quiz1/quiz1_review.md
+++ ruby_quiz/quiz1/quiz1_review.md
@@ -1,2 +1,11 @@
 使用
 a = %w{a b c d e f g h} 直接创建Array,注意用空格分隔
+
+## String.tr(from_str, to_str) 方法 ###
+
+去除字符串中除了某种串的方法
+    * ori_str.delete("^a-zA-Z0-9")
+    * ori_str.tr!('^A-Za-z','')_
+可以用 String.tr来对字符串进行大写处理
+    * ori_str.tr!('^A-Za-z','')
+    * ori_str.tr!('a-z','A-Z')
--- ruby_quiz/quiz1/solitaire_cipher.rb
+++ ruby_quiz/quiz1/solitaire_cipher.rb
@@ -2,7 +2,12 @@ class SolitaireCipher
   Version = "1.0"
   attr_accessor :ori_char, :ori_num, :abc_num_hash, :num_abc_hash
   @ori_char = %{A B C D E F G H I J K L M N O P Q R S T U V W X Y Z}
-  def initialize
+  def initialize(ori_str = "CODEI NRUBY LIVEL ONGER")
+    #ori_str.upcase
+    ori_str.tr!('^A-Za-z','')
+    ori_str.tr!('a-z','A-Z')
+    @str_length = ori_str.size
+
     abc_range = 'A'..'Z'
     @ori_char = abc_range.to_a
     @abc_num_hash = Hash.new -1
@@ -14,5 +19,13 @@ class SolitaireCipher
       index+=1
     end
   end
+
+  def gen_key
+    @key_stream = Array.new
+    
+  end
 end
+
+# discard any non A to Z characters
+# uppercase all chars
 
